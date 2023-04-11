f37186000f7d5e2ff9e16ff7ae335750fdea240a
Daniel Lusk <lusk@gfz-potsdam.dew>
2023-04-11 11:29:45 +0000

    prep for full plot

--- src/run_qsm.m
+++ src/run_qsm.m
@@ -1,11 +1,10 @@
 %% Load all point clouds and save in a single .mat file
-data_dir = "../../../data/clouds/2023-03-30_full_plot/11_12/leafoff/las";
+data_dir = "../../../2019_FrenchGuiana/TLS_segmentation/tls2trees/clouds/2023-03-30_full_plot/*/leafoff/las";
 files = dir(fullfile(data_dir, "*.las"));
-% Ps = cell(size(files, 1),1)
-clear Ps
+clear Ps;
 
 for i=1:size(files,1)
-    las = lasFileReader(fullfile(data_dir, files(i).name));
+    las = lasFileReader(fullfile(files(i).folder, files(i).name));
     pcd = readPointCloud(las);
     P = pcd.Location;
     P = P - mean(P); % Apply global shift
@@ -15,19 +14,20 @@ for i=1:size(files,1)
     Ps.("ti_" + tree_name) = P;
 end
 
-save("trees", "-struct", "Ps")
+save("paracou", "-struct", "Ps")
 
 %% Define the input parameters
 create_input();
-inputs = define_input('trees', 2, 3, 2)
+inputs = define_input('paracou', 2, 3, 2)
 
 %% Use |make_models| to produce QSMs
-QSMs = make_models_parallel('trees', 'QSMs_trees', 2, inputs);
+QSMs = make_models_parallel('paracou', 'QSMs_trees', 7, inputs, 'qsm_array_1');
 
 %% Select optimal QSMs
-[TreeData, OptModels, OptInputs, OptQSM] = select_optimum(QSMs);
+[TreeData, OptModels, OptInputs, OptQSM] = select_optimum(QSMs, 'opt_qsm_1');
+% save("qsm1", "TreeData", "OptModels", "OptInputs", "OptQSM")
 
 %% Estimate uncertainty more reliably
-QSMs2 = make_models_parallel('trees', 'QSMs_trees2', 20, OptInputs);
+QSMs2 = make_models_parallel('paracou', 'QSMs_trees2', 20, OptInputs, 'qsm_array_2');
 
-[TreeData, OptQSMs, OptQSM] = estimate_precision(QSMs, QSMs2, TreeData, OptModels);
+[TreeData, OptQSMs, OptQSM] = estimate_precision(QSMs, QSMs2, TreeData, OptModels, 'opt_qsm_2');

616971bc49ca8164a1f522747297921429dd3837
Daniel Lusk <lusk@gfz-potsdam.de>
2023-04-12 16:54:48 +0000

    read csv clouds instead

--- src/run_qsm.m
+++ src/run_qsm.m
@@ -1,12 +1,19 @@
+addpath(genpath("../src"));
+
 %% Load all point clouds and save in a single .mat file
-data_dir = "../../../2019_FrenchGuiana/TLS_segmentation/tls2trees/clouds/2023-03-30_full_plot/*/leafoff/las";
-files = dir(fullfile(data_dir, "*.las"));
+% data_dir = "../../../2019_FrenchGuiana/TLS_segmentation/tls2trees/clouds/2023-03-30_full_plot/*/leafoff/las";
+data_dir = "../../../2019_FrenchGuiana/TLS_segmentation/tls2trees/clouds/2023-03-30_full_plot/*/leafoff/csv"
+% data_dir = "data/clouds/2023-03-30_full_plot/11_12/leafoff/las";
+files = dir(fullfile(data_dir, "*.csv"));
 clear Ps;
 
 for i=1:size(files,1)
-    las = lasFileReader(fullfile(files(i).folder, files(i).name));
-    pcd = readPointCloud(las);
-    P = pcd.Location;
+% for i=1:2
+    % las = lasFileReader(fullfile(files(i).folder, files(i).name));
+    % pcd = readPointCloud(las);
+    % P = pcd.Location;
+    P = readmatrix(fullfile(files(i).folder, files(i).name), 'NumHeaderLines', 1);
+    P = P(:,1:3);
     P = P - mean(P); % Apply global shift
     % Ps(i) = {P};
     tree_name = strsplit(files(i).name, ".");
@@ -14,20 +21,20 @@ for i=1:size(files,1)
     Ps.("ti_" + tree_name) = P;
 end
 
-save("paracou", "-struct", "Ps")
+save("paracou", "-struct", "Ps");
 
 %% Define the input parameters
 create_input();
-inputs = define_input('paracou', 2, 3, 2)
+inputs = define_input('paracou', 2, 3, 2);
 
 %% Use |make_models| to produce QSMs
-QSMs = make_models_parallel('paracou', 'QSMs_trees', 7, inputs, 'qsm_array_1');
+QSMs = make_models_parallel('paracou', 'QSMs_1', 5, inputs);
 
 %% Select optimal QSMs
-[TreeData, OptModels, OptInputs, OptQSM] = select_optimum(QSMs, 'opt_qsm_1');
+[TreeData, OptModels, OptInputs, OptQSM] = select_optimum(QSMs, 'all_mean_dis', '1');
 % save("qsm1", "TreeData", "OptModels", "OptInputs", "OptQSM")
 
 %% Estimate uncertainty more reliably
-QSMs2 = make_models_parallel('paracou', 'QSMs_trees2', 20, OptInputs, 'qsm_array_2');
+QSMs2 = make_models_parallel('paracou', 'QSMs_2', 10, OptInputs);
 
-[TreeData, OptQSMs, OptQSM] = estimate_precision(QSMs, QSMs2, TreeData, OptModels, 'opt_qsm_2');
+[TreeData, OptQSMs, OptQSM] = estimate_precision(QSMs, QSMs2, TreeData, OptModels, '2');

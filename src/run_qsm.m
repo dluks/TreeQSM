02c2eee4ec35f675d70715654324e980610fcd6e
Daniel Lusk <lusk@gfz-potsdam.de>
2023-04-14 06:56:46 +0000

    clean up; run from 0.3

--- src/run_qsm.m
+++ src/run_qsm.m
@@ -1,27 +1,19 @@
 addpath(genpath("../src"));
-clear Ps;
 
 num_models_1 = 5;
 num_models_2 = 10;
 
 %% Load all point clouds and save in a single .mat file
-% data_dir = "../../../2019_FrenchGuiana/TLS_segmentation/tls2trees/clouds/2023-03-30_full_plot/*/leafoff/csv"
-% files = dir(fullfile(data_dir, "*.csv"));
 data_dir = "../../../2019_FrenchGuiana/TLS_segmentation/tls2trees/clouds/2023-03-30_full_plot";
 dirs = dir(data_dir);
 
-% for i=1:6
 for i=1:size(dirs, 1)
-    if dirs(i).isdir && (dirs(i).name ~= "." && dirs(i).name ~= ".." && dirs(i).name ~= "._.DS_Store")
+    if dirs(i).isdir && (dirs(i).name ~= "." && dirs(i).name ~= ".." && dirs(i).name ~= "._.DS_Store" && dirs(i).name ~= "0.1" && dirs(i).name ~= "0.2")
         files = dir(fullfile(dirs(i).folder, dirs(i).name, "leafoff/csv/*.csv"));
         if size(files, 1) > 0
             disp(strcat("Processing directory '", dirs(i).name, "' with ", string(size(files, 1)), " trees"));
-            % for j=1:2
+            clear Ps;
             for j=1:size(files, 1)
-                disp(strcat("Reading tree", files(j).name, "..."));
-                % las = lasFileReader(fullfile(files(i).folder, files(i).name));
-                % pcd = readPointCloud(las);
-                % P = pcd.Location;
                 P = readmatrix(fullfile(files(j).folder, files(j).name), 'NumHeaderLines', 1);
                 P = P(:,1:3);
                 P = P - mean(P); % Apply global shift
@@ -31,8 +23,8 @@ for i=1:size(dirs, 1)
             end
             
             disp("Saving clouds .mat...");
-            qsm_name = strcat("paracou_", dirs(i).name);
-            qsm_name = strrep(qsm_name,".","_");
+            dir_label = strrep(dirs(i).name, ".", "-");
+            qsm_name = strcat("paracou_", dir_label);
             save(qsm_name, "-struct", "Ps");
 
             %% Define the input parameters
@@ -42,19 +34,19 @@ for i=1:size(dirs, 1)
 
             %% Use |make_models| to produce QSMs
             disp("Making models...")
-            QSMs = make_models_parallel(qsm_name, strcat("QSMs_", dirs(i).name, "_1"), num_models_1, inputs);
+            QSMs = make_models_parallel(qsm_name, strcat("QSMs_", dir_label, "_1"), num_models_1, inputs);
 
             %% Select optimal QSMs
             disp("Selecting optimal QSMs...");
-            [TreeData, OptModels, OptInputs, OptQSM] = select_optimum(QSMs, 'all_mean_dis', strcat(dirs(i).name, "_1"));
+            [TreeData, OptModels, OptInputs, OptQSM] = select_optimum(QSMs, 'all_mean_dis', strcat(dir_label, "_1"));
             % save("qsm1", "TreeData", "OptModels", "OptInputs", "OptQSM")
 
-            %% Estimate uncertainty more reliably
-            disp("Making models with optimal input parameters for uncertainty estimation...");
-            QSMs2 = make_models_parallel(qsm_name, strcat("QSMs_", dirs(i).name, "_2"), num_models_2, OptInputs);
+            % %% Estimate uncertainty more reliably
+            % disp("Making models with optimal input parameters for uncertainty estimation...");
+            % QSMs2 = make_models_parallel(qsm_name, strcat("QSMs_", dir_label, "_2"), num_models_2, OptInputs);
 
-            disp("Estimating precision...");
-            [TreeData, OptQSMs, OptQSM] = estimate_precision(QSMs, QSMs2, TreeData, OptModels, strcat(dirs(i).name, "_2"));
+            % disp("Estimating precision...");
+            % [TreeData, OptQSMs, OptQSM] = estimate_precision(QSMs, QSMs2, TreeData, OptModels, strcat(dir_label, "_2"));
         else
             disp(strcat("Skipped empty directory: ", dirs(i).name))
         end

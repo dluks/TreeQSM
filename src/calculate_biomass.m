716c232d8b1d9a7798d5da45d533e5ae1ee6ec4d
Daniel Lusk <lusk@gfz-potsdam.de>
2023-10-05 11:00:12 +0000

    generalize for sharing

--- src/calculate_biomass.m
+++ src/calculate_biomass.m
@@ -1,31 +1,54 @@
-qsms_dir = './results';
-qsms = dir('./results/OptimalQSMs*.mat');
+qsm_dir = "location/of/qsms";
+qsms = dir(fullfile(qsm_dir, "qsms/optimal", "OptimalQSMs*.mat"));
 
-headings = {'biomass [g]', 'total volume [cm3]', 'x', 'y', 'z'};
-
-data = [];
-
-% for i=1:2
 for i=1:size(qsms)
 
     if isempty(strfind(qsms(i).name, '0-1'))
-        opt_qsms = load(fullfile(qsms(i).folder, qsms(i).name));
-        opt_qsms = opt_qsms.OptQSM;
+        opt_qsms_all = load(fullfile(qsms(i).folder, qsms(i).name));
+
+        diam = qsms(i).name(13:15);
+        % Update this to the correct filename pattern
+        shifts = load(fullfile(qsm_dir, "clouds", strcat("prefix_", diam, "_shifts.mat")));
+
+        opt_qsms = opt_qsms_all.OptQSM;
         qsms_data = NaN(size(opt_qsms, 2), 5);
         for j=1:size(opt_qsms, 2)
             opt_qsm = opt_qsms(j);
+            name = string(opt_qsms_all.TreeData(j).name);
             treedata = opt_qsm.treedata;
+
+            shift = [shifts.(name)];
+            location = treedata.location + shift;
+
+            % Get volume from branches > 0.03 m
+            branches = opt_qsm.branch;
+            total_vol = 0;
+            branch_vol = 0;
+            for k=1:size(branches.diameter, 1)
+                if branches.order(k) > 0 && branches.diameter(k) >= 0.03
+                    branch_vol = branch_vol + branches.volume(k);
+                end
+            end
             
-            total_vol = treedata.TotalVolume;
-            location = treedata.location;
+            total_vol = branch_vol + opt_qsm.treedata.TrunkVolume;
             biomass = total_vol * 0.626;
 
-            row = [biomass, total_vol, location(1), location(2), location(3)];
-            qsms_data(j, :) = row;
+            tree.id = name;
+            tree.easting = location(1);
+            tree.northing = location(2);
+            tree.elevation = location(3);
+            tree.volume = total_vol;
+            tree.biomass = biomass;
+
+            if ~exist("trees")
+                trees = tree;
+            else
+                trees = [trees, tree];
+            end
         end
-        data = [data; qsms_data];
     end
 end
 
-T = array2table(data, 'VariableNames', headings);
-writetable(T, "../../../2019_FrenchGuiana/TLS_segmentation/tls2trees/qsm/paracou_biomass.csv");
+T = struct2table(trees);
+% Update the below output path according to your needs
+writetable(T, "path/to/biomass.csv");
--- src/run_qsm.m
+++ src/run_qsm.m
@@ -4,15 +4,13 @@ num_models_1 = 5;
 num_models_2 = 10;
 
 %% Load all point clouds and save in a single .mat file
-data_dir = "../../../2019_FrenchGuiana/TLS_segmentation/tls2trees/clouds/2023-03-30_full_plot";
-odir = "../../../2019_FrenchGuiana/TLS_segmentation/tls2trees/qsm/clouds";
+data_dir = "path/to/point/clouds";
+odir = "path/to/output/directory";
 
 dirs = dir(data_dir);
 
 for i=1:size(dirs, 1)
-    % if dirs(i).isdir && (dirs(i).name ~= "." && dirs(i).name ~= ".." && dirs(i).name ~= "._.DS_Store" && dirs(i).name ~= "0.1" && dirs(i).name ~= "0.2")
-    if str2double(dirs(i).name) >= 0.0 && str2double(dirs(i).name) <= 2.2
-    % if str2double(dirs(i).name) == 0.9
+    if dirs(i).isdir && (dirs(i).name ~= "." && dirs(i).name ~= ".." && dirs(i).name ~= "._.DS_Store")
         files = dir(fullfile(dirs(i).folder, dirs(i).name, "leafoff/csv/*.csv"));
         if size(files, 1) > 0
             disp(strcat("Processing directory '", dirs(i).name, "' with ", string(size(files, 1)), " trees"));
@@ -21,40 +19,42 @@ for i=1:size(dirs, 1)
             for j=1:size(files, 1)
                 P = readmatrix(fullfile(files(j).folder, files(j).name), 'NumHeaderLines', 1);
                 P = P(:,1:3);
-                P = P - mean(P); % Apply global shift
+                shift = mean(P);
+                P = P - shift; % Apply global shift
                 tree_name = strsplit(files(j).name, ".");
                 tree_name = tree_name(1);
-                % Ps.("ti_" + tree_name) = P;
-                shifts.("ti_" + tree_name) = mean(P);
+                Ps.("ti_" + tree_name) = P;
+                shifts.("ti_" + tree_name) = shift;
             end
             
-            % disp("Saving clouds .mat...");
+            disp("Saving clouds .mat...");
             dir_label = strrep(dirs(i).name, ".", "-");
-            qsm_name = strcat("paracou_", dir_label);
-            qsm_file = fullfile(odir, qsm_name);
-            % save(qsm_file, "-struct", "Ps");
+            cloud_name = strcat("paracou_", dir_label);
+            cloud_file = fullfile(odir, cloud_name);
+            save(cloud_file, "-struct", "Ps");
             
             disp("Saving global shifts...");
-            shifts_file = fullfile(odir, strcat(qsm_name, "_shifts"));
+            shifts_file = fullfile(odir, strcat(cloud_name, "_shifts"));
             save(shifts_file, "-struct", "shifts");
 
-            % %% Define the input parameters
-            % disp("Defining input parameters...");
-            % create_input();
-            % inputs = define_input(qsm_name, 2, 3, 2);
+            %% Define the input parameters
+            disp("Defining input parameters...");
+            create_input();
+            inputs = define_input(cloud_name, 2, 3, 2);
 
-            % %% Use |make_models| to produce QSMs
-            % disp("Making models...")
-            % QSMs = make_models_parallel(qsm_name, strcat("QSMs_", dir_label, "_1"), num_models_1, inputs);
+            %% Use |make_models| to produce QSMs
+            disp("Making models...")
+            qsm_file = fullfile(odir, strcat("QSMs_", cloud_name));
+            QSMs = make_models_parallel(cloud_file, qsm_file, num_models_1, inputs);
 
-            % %% Select optimal QSMs
-            % disp("Selecting optimal QSMs...");
-            % [TreeData, OptModels, OptInputs, OptQSM] = select_optimum(QSMs, 'all_mean_dis', strcat(dir_label, "_1"));
-            % save("qsm1", "TreeData", "OptModels", "OptInputs", "OptQSM")
-
-            % %% Estimate uncertainty more reliably
+            %% Select optimal QSMs
+            disp("Selecting optimal QSMs...");
+            opt_qsm_file = fullfile(odir, strcat("OptimalQSMs_", cloud_name));
+            [TreeData, OptModels, OptInputs, OptQSM] = select_optimum(QSMs, 'all_mean_dis', opt_qsm_file);
+            
+            %% Estimate uncertainty more reliably (this can take a while) (optional)
             % disp("Making models with optimal input parameters for uncertainty estimation...");
-            % QSMs2 = make_models_parallel(qsm_name, strcat("QSMs_", dir_label, "_2"), num_models_2, OptInputs);
+            % QSMs2 = make_models_parallel(cloud_name, strcat("QSMs_", dir_label, "_2"), num_models_2, OptInputs);
 
             % disp("Estimating precision...");
             % [TreeData, OptQSMs, OptQSM] = estimate_precision(QSMs, QSMs2, TreeData, OptModels, strcat(dir_label, "_2"));
